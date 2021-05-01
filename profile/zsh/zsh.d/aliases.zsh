3bda41f9158ae5a2c88091384a071c7a63570757
cosckoya <javier.coscolla@mail.me>
2021-05-01 23:21:03 +0200

    ZSH repaired

--- profile/zsh/zsh.d/aliases.zsh
+++
@@ -1,4 +0,0 @@
-# ZSH Aliases
-alias vi='nvim'
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
--- profile/zsh/zsh.d/plugins.zsh
+++
@@ -1,8 +0,0 @@
-# Plugins
-
-# Kubectl
-if [[ -d $HOME/.kube ]]; then
-  zinit load superbrothers/zsh-kubectl-prompt
-  zstyle ':zsh-kubectl-prompt:' separator '|'
-  RPROMPT='%F{#af00ff%}($ZSH_KUBECTL_PROMPT)%{$reset_color%}'
-fi
