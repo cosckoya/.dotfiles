4f30b3156eb6824fe16020f7cabb721659a08090
cosckoya <javier.coscolla@mail.me>
2021-06-30 21:31:56 +0200

    If.. Else testing

--- profile/zsh.d/autocomplete.zsh
+++ profile/zsh.d/autocomplete.zsh
@@ -1,6 +1,8 @@
-# Autocomplete
+##
+## Autocomplete
+##
 
-if [[ -d $HOME/.asdf ]]; then                                                                  # asdf
+if [[ -d $HOME/.asdf ]]; then                                                  # asdf
   source $HOME/.asdf/asdf.sh && \
   source $HOME/.asdf/completions/asdf.bash && \
   if [[ $(uname -m) != "x86_64" ]]; then
@@ -8,13 +10,29 @@ if [[ -d $HOME/.asdf ]]; then                                                   
   fi
 fi
 
-if [[ -x $(asdf which gcloud) ]]; then                                                         # Google
+##
+# Cloud
+##
+
+if [[ -x $(asdf which gcloud) ]]; then                                         # Google
   source $(asdf which gcloud | awk -F '/bin' '{print $1}')/completion.zsh.inc
 fi
 
-if [[ -x $(which kubectl) ]]; then source <(kubectl completion zsh); fi                        # Kubectl
-if [[ -x $(which helm) ]]; then source <(helm completion zsh); fi                              # Helm
-if [[ -x $(which kind) ]]; then source <(kind completion zsh); fi                              # Kind
-if [[ -x $(which terraform) ]]; then complete -o nospace -C $(which terraform) terraform; fi   # Terraform
-if [[ -x $(which aws) ]]; then complete -C $(which aws_completer) aws; fi                      # AWS
-if [[ -x $(which az) ]]; then source "$HOME/.zsh.d/az.completion"; fi                   # Azure
+if [[ -x $(which aws) ]]; then complete -C $(which aws_completer) aws; fi      # AWS
+if [[ -x $(which az) ]]; then source "$HOME/.zsh.d/az.completion"; fi          # Azure
+
+##
+# Kubernetes
+## 
+
+if [[ -x $(which kubectl) ]]; then source <(kubectl completion zsh); fi        # Kubectl
+if [[ -x $(which helm) ]]; then source <(helm completion zsh); fi              # Helm
+if [[ -x $(which kind) ]]; then source <(kind completion zsh); fi              # Kind
+
+##
+# IaC
+##
+
+if [[ -x $(which terraform) ]]; then
+  complete -o nospace -C $(which terraform) terraform                          # Terraform
+fi
