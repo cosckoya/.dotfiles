c279de8d26ff4a12410cdabba2f4c2fa85b4a5c8
cosckoya <javier.coscolla@mail.me>
2023-11-05 14:11:48 +0100

    Added: Git, Tools

--- profile/zsh.d/alias.zsh
+++ profile/zsh.d/alias.zsh
@@ -2,8 +2,5 @@
 
 if command -v docker >/dev/null 2>&1; then
   alias kali='docker run --rm -ti kalilinux/kali-rolling bash'
-  alias ubuntu='docker run --rm -ti ubuntu:latest bash'
   alias debian='docker run --rm -ti debian:latest bash'
-  alias centos='docker run --rm -ti centos:latest bash'
-  alias archlinux='docker run --rm -ti archlinux:latest bash'
 fi
