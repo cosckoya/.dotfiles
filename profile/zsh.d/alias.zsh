55c8877dd961e579c7cdf9e24e95722c8d29bc6c
cosckoya <javier.coscolla@mail.me>
2024-07-30 02:46:17 +0200

    Updating...

--- profile/zsh.d/alias.zsh
+++ profile/zsh.d/alias.zsh
@@ -2,5 +2,6 @@
 
 if command -v docker >/dev/null 2>&1; then
   alias kali='docker run --rm -ti kalilinux/kali-rolling bash'
+  alias parrot='docker run --rm -ti parrotsec/core bash'
   alias debian='docker run --rm -ti debian:latest bash'
 fi
